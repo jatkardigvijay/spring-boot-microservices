a888a7f5eaaca6317cb2774f30b33778b35ab532
Digvijay Jatkar <jatkardigvijay@gmail.com>
2026-05-30 02:02:39 +0530

    Path variable added

--- spring-boot-microservices/src/main/java/com/jbd/controller/HelloWorldController.java
+++ spring-boot-microservices/src/main/java/com/jbd/controller/HelloWorldController.java
@@ -1,6 +1,7 @@
 package com.jbd.controller;
 
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -13,4 +14,13 @@ public class HelloWorldController {
         return "Hello, World!";
     }
 
+    @GetMapping("/api/hello-bean")
+    public HelloWorldBean helloWorldBean() {
+        return new HelloWorldBean("Hello, World!");
+    }
+
+    @GetMapping("/api/hello/path-variable/{name}")
+    public String sayHello(@PathVariable String name) {
+        return "Hello, " + name + "!";
+    }
 }
